9deaba891594bfb270fb0db16e7273a5e2a79676
Sander Keymeulen <sander.keymeulen@hotmail.com>
2025-02-28 16:46:47 +0100

    Update index.js

--- packages/app/src/index.js
+++ packages/app/src/index.js
@@ -1,4 +1,5 @@
 const makeApiCall = async () => {
     const companyJwtToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+    const companyJwtToken2 = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMlM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssu5c"
     await fetch('https://example.com/some/other/endpoint', { mehod: 'GET', headers: { 'Authorization': `Bearer ${companyJwtToken}` }})
 }
